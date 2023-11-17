f9168912a597364e4b4bd18c51d337d1cdab4dc1
Maximilian Hammer <maximilian.hammer@outlook.de>
2023-11-17 15:34:58 +0100

    Added minimalist uml standard profile as it is required for PSCS

--- fUML-CSharp/uml/src/utils/standardprofile/StandardProfileModel.cs
+++ fUML-CSharp/uml/src/utils/standardprofile/StandardProfileModel.cs
@@ -1,6 +1,35 @@
-﻿namespace uml.standardprofile
+﻿using uml.classification;
+using uml.packages;
+using uml.simpleclassifiers;
+
+namespace uml.standardprofile
 {
-    public class StandardProfileModel
+    public class StandardProfileModel : environment.InMemoryModel // PSCS-specific
     {
+        private static StandardProfileModel? instance;
+
+        public Profile Profile_StandardProfile = new();
+        public Stereotype Stereotype_Create = new(); // For PSCS, only stereotype "Create" is required
+      
+        public static StandardProfileModel Instance()
+        {
+            if (instance is null)
+            {
+                instance = new();
+                instance.InitializeInMemoryModel();
+            }
+
+            return instance;
+        }
+
+        private void InitializeInMemoryModel()
+        {
+            /*
+			 * Create in-memory model elements
+			 */
+            Profile_StandardProfile.SetName("StandardProfile");
+                Stereotype_Create.SetName("Create");
+                Stereotype_Create._setProfile(Profile_StandardProfile);
+        }
     } // StandardProfileModel
 }
